4854071c2ed6e9bac25e3ecb91807a4770dc3074
kubotak0630 <kubotak0630@gmail.com>
2022-04-23 07:24:34 +0900

    first commit

---
+++ src/components/ClstElementV.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="clst">
+    <el-button type="primary">{{ clstNo }}</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ClstElementV',
+  components: {},
+  props: {
+    clstNo: {
+      type: Number,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.clst {
+  margin: 0 2px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <NandClusterV />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import NandClusterV from '@/components/NandClusterV.vue';
 
 export default defineComponent({
   name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
+  components: { NandClusterV },
 });
 </script>
